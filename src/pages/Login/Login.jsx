a9b0fcfbafca9dd4a46a54da4cfd1c11a4cea646
protim1451 <partha.doict@gmail.com>
2024-04-06 13:09:41 +0600

    new directory

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>This is login page</h2>
+        </div>
+    );
+};
+
+export default Login;
